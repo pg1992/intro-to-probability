bfb1177b9a1ff464c68c6029387a9dc9e8b68ff8
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2022-01-23 14:29:16 -0300

    Fix linting errors

--- ch01/sec01/CoinTosses.py
+++ ch01/sec01/CoinTosses.py
@@ -9,7 +9,8 @@ questions 1 and 2 from section 1.1 of the book
 import random as rnd
 import sys
 
-def printTosses(maxTosses : int = 1000, interval : int = 100):
+
+def printTosses(maxTosses: int = 1000, interval: int = 100):
     """A random experiment showing the behavior of coin tosses.
 
     This routine shows how the proportion of heads minus 0.5 behaves
@@ -25,20 +26,29 @@ def printTosses(maxTosses : int = 1000, interval : int = 100):
     num_heads = 0.
     num_tails = 0.
     for i in range(1, maxTosses + 1):
-        if rnd.randrange(2) == 0: num_heads += 1.
-        else: num_tails += 1.
+        if rnd.randrange(2) == 0:
+            num_heads += 1.
+        else:
+            num_tails += 1.
         if i % interval == 0:
-            print('{1:06d}: Proportion of heads minus 1/2 = {0:.3f}.'.format(num_heads/i - .5, i))
-            print('{1:06d}: Number of heads minus 1/2 the number of tosses = {0:.3f}.'.format(num_heads - .5 * i, i))
+            prop = num_heads / i - .5
+            print(f'{i:06d}: Proportion of heads minus 1/2 = {prop:.3f}.')
+
+            prop *= i
+            print(f'{i:06d}: # of heads minus 1/2 the # of tosses: {prop:d}.')
             print('-' * 80)
 
-def printSampleSize(num_exp : int = 100, max_tosses : int = 100, num_trials : int = 100, prop : float = 0.95, dev : float = 0.1):
-    """Find the average number of coin tosses required so that the proportion of heads lies in the confidence interval.
+
+def printSampleSize(num_exp: int = 100, max_tosses: int = 100,
+                    num_trials: int = 100, prop: float = 0.95,
+                    dev: float = 0.1):
+    """Find the average number of coin tosses required so that the proportion
+    of heads lies in the confidence interval.
 
     Run 'num_exp' experiments with 'num_trials' trials each. In each experiment
     the number of coin tosses increases from 1 to 'max_tosses'.
-    The minimum number of tosses necessary so that a proportion 'prop' of trials
-    has the proportion of heads lying within 'dev' of 0.5 is stored.
+    The minimum number of tosses necessary so that a proportion 'prop' of
+    trials has the proportion of heads lying within 'dev' of 0.5 is stored.
     At the end of the routine the minimum number of tosses is averaged over
     all experiments.
     """
@@ -52,10 +62,13 @@ def printSampleSize(num_exp : int = 100, max_tosses : int = 100, num_trials : in
                 num_heads = 0.
                 num_tails = 0.
                 for toss in range(num_tosses):
-                    if rnd.randrange(2) == 0: num_heads += 1.
-                    else: num_tails += 1.
+                    if rnd.randrange(2) == 0:
+                        num_heads += 1.
+                    else:
+                        num_tails += 1.
                 prop_heads = num_heads / (num_heads + num_tails)
-                if abs(prop_heads - 0.5) <= dev: in_range += 1
+                if abs(prop_heads - 0.5) <= dev:
+                    in_range += 1
             if in_range/num_trials >= prop:
                 average_min_num_toss += num_tosses
                 break
@@ -64,7 +77,10 @@ def printSampleSize(num_exp : int = 100, max_tosses : int = 100, num_trials : in
     print('Number of trials per experiment:', num_trials)
     print('Target proportion of successful trials:', prop)
     print('Proportion of heads considered success:', 'within', dev, 'of', 0.5)
-    print('Average of the minimum number of tosses per trial over all experiments that reaches the target proportion of successful trials:', average_min_num_toss)
+    print('Average of the minimum number of tosses per trial over all '
+          'experiments that reaches the target proportion of successful '
+          'trials:', average_min_num_toss)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -85,5 +101,4 @@ if __name__ == "__main__":
 
             printTosses(numTosses, interval)
         elif ex_num == 2:
-            printSampleSize(dev = 0.1)
-
+            printSampleSize(dev=0.1)
