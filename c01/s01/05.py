5e1b44c5cc3b9894cbe7eb28d25c2e4da6f39ac5
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2022-01-23 23:39:33 -0300

    Simulate problem 1.1.5 also

--- c01/s01/05.py
+++ c01/s01/05.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 
+def proportion(trials, throws, dice=3) -> float:
+    sim = np.random.choice(6, (trials, throws, dice)) + 1
+
+    return (sim == 6).all(axis=2).any(axis=1).sum() / trials
+
+
 if __name__ == "__main__":
+    # Increase the number of throws until the probability of failure passe 0.5
     p = 1/6
     n_dice = 3
     p_success = p**n_dice
@@ -12,5 +19,15 @@ if __name__ == "__main__":
         n += 1
         p_fail *= (1 - p_success)
 
-    print('simulating, we get', n)
-    print('n must be greater than', -1/np.log2(1-p**n_dice))
+    print('The probability of failure becomes < .5 after', n, 'throws.')
+
+    # Explicit calculation
+    print('Explicit calculation: n >', -1 / np.log2(1 - p**n_dice))
+
+    # Simulate (a.k.a. Monte Carlo)
+    throws = 1
+    trials = 10000
+    while proportion(trials, throws, n_dice) < .5:
+        throws += 1
+    print(f"At least {n_dice} sixes appeared more than half of {trials} "
+          f"games if we throw {n_dice} dice {throws} times at each game.")
