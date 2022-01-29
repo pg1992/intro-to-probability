3da843a62770523c681214de76c5412186f80f46
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2022-01-29 01:27:58 -0300

    Estimate pi by Monte Carlo method
    We find the proportion of points that lies inside the circle
    with radius .5 and center at [.5, .5] when chosen at random
    inside the unit square.

--- c02/s01/exercises/README.md
+++ c02/s01/exercises/README.md
@@ -5,3 +5,12 @@ Problems 1 and 2
 ----------------
 
 The program [`01_02.py`](01_02.py) simulates the spinner.
+
+Problem 3
+---------
+
+The program [`monte_carlo.py`](monte_carlo.py) finds the proportion of points
+that lie in a region and we can estimate the area.  By counting only the numbers
+that lie in the circle with radius .5 (i.e., (x-.5)^2 + (y-.5)^2 <= .5^2), we
+can estimate pi (approx. 4 * proportion).  Running the simulation with 1,000
+points we get values with .05 error.
---
+++ c02/s01/exercises/monte_carlo.py
@@ -0,0 +1,37 @@
+"""Monte Carlo
+
+Estimate areas by running a Monte Carlo simulation inside the unit square
+with different criterias.
+"""
+
+import numpy as np
+
+
+def monte_carlo(criteria, num_points) -> float:
+    """Area estimation by Monte Carlo
+
+    Generate `num_points` points and see if they satisfy `criteria`.
+    Returns the proportion of points that satisfy the criteria.
+
+    All points must lie in the unit square with corners [0, 0], [0, 1], [1, 0],
+    and [1, 1].
+    """
+
+    samples = np.random.sample((num_points, 2))
+    in_area = np.sum(criteria(samples[:, 0], samples[:, 1]))
+
+    return in_area / num_points
+
+
+if __name__ == "__main__":
+    num_points = 1000
+
+    def circle_criteria(x, y):
+        """Circle with radius .5 and center [.5, .5]."""
+
+        return (x - .5)**2 + (y - .5)**2 <= .5**2
+
+    proportion = monte_carlo(circle_criteria, num_points)
+    pi_estimate = 4 * proportion
+
+    print(f"P3: proportion = {proportion}; pi_estimate = {pi_estimate}.")
