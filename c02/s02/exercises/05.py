9f261494aff1c1a79bd1ca7e4d9ce7ba3173ba28
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2022-02-14 16:49:24 -0300

    Find the probability that a coin turns head a number of times

---
+++ c02/s02/exercises/05.py
@@ -0,0 +1,23 @@
+"""Probability of number coin throws
+
+Given n throws of a coin, what is the probability that the number of heads
+lies between some interval.
+"""
+
+import numpy as np
+from scipy.special import comb
+
+n = 100
+p = .5
+k = np.arange(n + 1)
+b = comb(n, k) * p**k * (1 - p)**(n - k)
+
+limits = [
+    (35, 65),
+    (40, 60),
+    (45, 55),
+]
+
+for lmin, lmax in limits:
+    prob = np.round(np.sum(b[(lmin <= k) & (lmax <= 65)]), 3)
+    print(f'P({lmin} <= n <= {lmax}) = {prob}')
